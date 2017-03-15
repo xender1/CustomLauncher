0564f194eb572a7c8794691e169fe7b22e00ee7c
Curtis Keilty <Curtis Keilty>
2017-03-15 00:44:39 -0400

    Add project files.

---
+++ CustomLauncher/CKTestClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CustomLauncher
+{
+    public class CKTestClass
+    {
+        public int x, y;
+        public string firstname;
+        public int age;
+        public string lastname;
+        public string display;
+
+        public CKTestClass()
+        {
+            x = 1;
+            y = 2;
+            firstname = "Curtis";
+            age = 26;
+            lastname = "Keilty";
+            display = "Initialized";
+        }
+
+        public CKTestClass(int x)
+        {
+            x = 1;
+            y = 2;
+            firstname = "Curtis";
+            age = 26;
+            lastname = "Keilty";
+        }
+
+        public void SetFirstName(string input)
+        {
+            firstname = input;
+        }
+
+        public string GetName()
+        {
+            return firstname;
+        }
+
+        public string Display()
+        {
+            display = firstname + " " + lastname + ", age: " + age + "\nx: " + x + " y: " + y;
+            return display;
+        }
+
+        public void innerAddX()
+        {
+            x = x + 1;
+
+        }
+
+        public void innerAddY()
+        {
+            y = y + 2;
+        }
+
+        public void IncreaseAge()
+        {
+            age = age + 1;
+        }
+
+        public void ChangeNames()
+        {
+            if (firstname == "Curtis")
+            {
+                firstname = "Bob";
+                lastname = "Ross";
+            } else
+            {
+                firstname = "Curtis";
+                lastname = "Keilty";
+            }
+        }
+
+        public static int myAdd(int a, int b)
+        {
+            int ret = a + b;
+            return ret;
+        }
+    }
+}
---
+++ CustomLauncher/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CustomLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int globalX = 2;
+        int globalY = 1;
+
+        //CKTestClass myObj = new CKTestClass();
+        //CKTestClass myObjTwo = new CKTestClass();
+
+        //do one as lists and other as hardcoded array size 2
+        List<CKTestClass> myList = new List<CKTestClass>();
+        int val = 0;
+
+        DispatcherTimer myTimerThread = new DispatcherTimer();
+        int timerVal = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myList.Add(new CKTestClass());
+            myList.Add(new CKTestClass());
+
+            labelListInfo.Content = myList[0].Display() + "\n" + myList[1].Display();
+
+
+            myTimerThread.Tick += new EventHandler(myTimerThread_Tick);
+            myTimerThread.Interval = new TimeSpan(0, 0, 1);
+            myTimerThread.Start();
+
+            /*
+            new Thread(() =>
+            {
+                while(true)
+                {
+                    //secondsPassed = System.ctim
+                }
+            }).Start();
+            */
+        }
+
+        public void myTimerThread_Tick(object sender, EventArgs e)
+        {
+            if (myList.Count == 2)
+            {
+                timerVal = timerVal + 1;
+            }
+
+            labelMiscInfo.Content = timerVal;
+        }
+
+        private string DisplayListValues()
+        {
+            string ret = "";
+            foreach (CKTestClass item in myList)
+            {
+                ret += item.Display() + "\n";
+            }
+            return ret;
+        }
+
+        /**
+         * List Functions
+         ***/
+
+        private void buttonListAddNewListItem_Click(object sender, RoutedEventArgs e)
+        {
+            myList.Add(new CKTestClass());
+            labelListInfo.Content = DisplayListValues();
+        }
+
+        private void buttonListAddX_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (int.TryParse(textInputListEntry.Text, out val))
+            {
+                myList[val].innerAddX();
+                labelListInfo.Content = DisplayListValues();
+            }
+
+            //myTimerThread.
+        }
+
+        private void buttonListAddY_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(textInputListEntry.Text, out val))
+            {
+                myList[val].innerAddY();
+                labelListInfo.Content = DisplayListValues();
+            }
+        }
+
+        private void buttonListAddAge_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(textInputListEntry.Text, out val))
+            {
+                myList[val].IncreaseAge();
+                labelListInfo.Content = DisplayListValues();
+            }
+        }
+
+        private void buttonListChangeName_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(textInputListEntry.Text, out val))
+            {
+                myList[val].SetFirstName(textInputName.Text);
+                labelListInfo.Content = DisplayListValues();
+            }
+        }
+    }
+
+}
